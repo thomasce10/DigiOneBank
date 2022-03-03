fb1d26a565ac7fe7a7e2edfb6c354d4555daa738
Thomas Brito <thomasce_10@hotmail.com>
2022-03-03 17:16:45 -0300

    implementando classe People.kt

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/People.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank
+
+class People {
+    var name: String = "Thomas"
+    var cpf: String = "084.234.132.67"
+}
+
+fun main(){
+    val jether = People()
+
+    println(jether.name)
+    println(jether.cpf)
+}
